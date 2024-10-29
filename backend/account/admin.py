b859ad8049246576ecba702137851d36042b52c2
YOOHOO <yoohoo_030@naver.com>
2024-10-29 21:49:15 +0900

    chore: admin페이지 중 user 목록 페이지 상세화

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import User, Teacher, Student
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'name', 'uid']
+
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Teacher)
-admin.site.register(Student)
+admin.site.register(User, UserAdmin)
+admin.site.register(Teacher, UserAdmin)
+admin.site.register(Student, UserAdmin)
