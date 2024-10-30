695b4f34ecaf9973aa339e494d2e0c332fe7b853
YOOHOO <yoohoo_030@naver.com>
2024-10-30 21:30:16 +0900

    test: mockdata 생성 함수 구현 및 코드 리팩토링

--- backend/account/tests/test_registration.py
+++ backend/account/tests/test_registration.py
@@ -1,49 +1,29 @@
 from rest_framework.test import APITestCase
-from ..factories import StudentFactory, TeacherFactory
 from rest_framework import status
-import random
+from .testdata import create_moke_user_data
 
 class UserRegisterTestCase(APITestCase):
-    def setUp(self):
-        self.url = '/users/register/'
+    @classmethod
+    def setUpTestData(cls):
+        cls.url = '/users/register/'        
 
-    def create_user_data(self, **kwargs):
-        choice = random.choice([1, 2]) # 선생님과 학생 중 테스트할 모델을 랜덤으로 선택
-        if choice == 1:
-            user_factory = TeacherFactory
-            role = "선생님"
-        elif choice == 2:
-            user_factory = StudentFactory
-            role = "학생"
-        user = user_factory.build() # db에 저장되지 않게 설정
-        data = {
-            "name": user.name,
-            "email": user.email,
-            "role": role,
-            "password": user.password,
-        }
-        # 유저 타입에 따라 추가 필드 설정
-        if role == '학생':
-            data['school'] = user.school
-            data['grade'] = user.grade
-        elif role == '선생님':
-            data['institution'] = user.institution
-            data['subject'] = user.subject
-        data.update(kwargs)
-        return data
+    def edit_moke_user(self, **kwargs):
+        moke_user = create_moke_user_data()
+        moke_user.update(kwargs)
+        return moke_user
 
     def test_successful_registration(self):
-        data = self.create_user_data()
-        response = self.client.post(self.url, data=data, format='json')
+        moke_user = self.edit_moke_user()
+        response = self.client.post(self.url, data=moke_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_register_with_missing_email(self):
-        data = self.create_user_data(email="")
-        response = self.client.post(self.url, data=data, format='json')
+        moke_user = self.edit_moke_user(email="")
+        response = self.client.post(self.url, data=moke_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_register_with_duplicate_email(self):
-        existing_user = self.create_user_data()
+        existing_user = self.edit_moke_user()
         self.client.post(self.url, data=existing_user, format='json')
         new_user = {
             "email": existing_user['email'],
@@ -57,66 +37,66 @@ class UserRegisterTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_register_with_invalid_email_format(self):
-        data = self.create_user_data(email='abcemail@')
-        response = self.client.post(self.url, data=data, format='json')
+        moke_user = self.edit_moke_user(email='abcemail@')
+        response = self.client.post(self.url, data=moke_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_register_with_missing_name(self):
-        data = self.create_user_data(name="")
-        response = self.client.post(self.url, data=data, format='json')
+        moke_user = self.edit_moke_user(name="")
+        response = self.client.post(self.url, data=moke_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         
     def test_register_with_short_password(self):
-        data = self.create_user_data(password="a76")
-        response = self.client.post(self.url, data=data, format='json')
+        moke_user = self.edit_moke_user(password="a76")
+        response = self.client.post(self.url, data=moke_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_register_with_unselected_fields(self):
-        data = self.create_user_data()
-        if data["role"] == "선생님":
-            data["institution"] = None
-            data["subject"] =  None
-        elif data["role"] == "학생":
-            data["school"] = None
-            data["grade"] = None
-        response = self.client.post(self.url, data=data, format='json')
+        moke_user = self.edit_moke_user()
+        if moke_user["role"] == "선생님":
+            moke_user["institution"] = None
+            moke_user["subject"] =  None
+        elif moke_user["role"] == "학생":
+            moke_user["school"] = None
+            moke_user["grade"] = None
+        response = self.client.post(self.url, data=moke_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_register_with_unselected_role(self):
-        data = self.create_user_data()
-        data["role"] = None
-        response = self.client.post(self.url, data=data, format='json')
+        moke_user = self.edit_moke_user()
+        moke_user["role"] = None
+        response = self.client.post(self.url, data=moke_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_register_with_invalid_role(self):
-        data = self.create_user_data()
-        data["role"] = "아뇨 뚱인데요"
-        response = self.client.post(self.url, data=data, format='json')
+        moke_user = self.edit_moke_user()
+        moke_user["role"] = "아뇨 뚱인데요"
+        response = self.client.post(self.url, data=moke_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_register_with_no_institution_or_school(self):
-        data = self.create_user_data()
-        if (data["role"] == "선생님"):
-            data["institution"] = ""
-        elif (data["role"] == "학생"):
-            data["school"] = ""
-        response = self.client.post(self.url, data=data, format='json')
+        moke_user = self.edit_moke_user()
+        if (moke_user["role"] == "선생님"):
+            moke_user["institution"] = ""
+        elif (moke_user["role"] == "학생"):
+            moke_user["school"] = ""
+        response = self.client.post(self.url, data=moke_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_register_with_no_subject_or_grade(self):
-        data = self.create_user_data()
-        if (data["role"] == "선생님"):
-            data["subject"] = ""
-        elif (data["role"] == "학생"):
-            data["grade"] = ""
-        response = self.client.post(self.url, data=data, format='json')
+        moke_user = self.edit_moke_user()
+        if (moke_user["role"] == "선생님"):
+            moke_user["subject"] = ""
+        elif (moke_user["role"] == "학생"):
+            moke_user["grade"] = ""
+        response = self.client.post(self.url, data=moke_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_register_with_invalid_subject_or_grade(self):
-        data = self.create_user_data()
-        if (data["role"] == "선생님"):
-            data["subject"] = "요리"
-        elif (data["role"] == "학생"):
-            data["grade"] = "대5"
-        response = self.client.post(self.url, data=data, format='json')
+        moke_user = self.edit_moke_user()
+        if (moke_user["role"] == "선생님"):
+            moke_user["subject"] = "요리"
+        elif (moke_user["role"] == "학생"):
+            moke_user["grade"] = "대5"
+        response = self.client.post(self.url, data=moke_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
