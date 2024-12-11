389cf99807669443be7317dcfac3758a86d9af1a
Tronze <0183535663@naver.com>
2024-12-12 04:30:08 +0900

    fix: url 뒤에 / 붙도록 수정.

--- frontend/src/apis/feedback.js
+++ frontend/src/apis/feedback.js
@@ -1,11 +1,11 @@
 import axios from "../axiosInstance";
 
 export const fetchFeedbackList = async () => {
-    const response = await axios.get("/feedbacks");
+    const response = await axios.get("/feedbacks/");
     return response.data;
 }
 
 export const fetchFeedbackDetail = async (uid) => {
-    const response = await axios.get(`/feedbacks/${uid}`);
+    const response = await axios.get(`/feedbacks/${uid}/`);
     return response.data;
 }
